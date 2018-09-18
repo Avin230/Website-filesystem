dcea1e2c093d02386f8db9d661c845721314a81f
Avin230 <avin.keith@hgs.hiddengeniusproject.org>
2018-09-17 18:22:04 -0700

    successful

---
+++ run.sh
@@ -0,0 +1,40 @@
+cd ~/Desktop 						#goes into Desktop folder
+mkdir website-filesystem			#creates website-filesystem folder
+cd website-filesystem				#goes into website-filesystem 
+touch index.html 					#creates index.html file
+mkdir html							#creates html folder
+cd html								#goes into html folder
+touch pg1.html						#creates pg1.html file
+touch pg2.html						#creates pg2.html file
+cd ../								#goes back a folder
+mkdir css							#creates css folder 
+cd css								#goes into css folder
+touch index.css						#creates index.css file
+touch pg1.css						#creates pg1.css file
+touch pg2.css						#creates pg2.css file
+cd ../								#goes back a folder
+mkdir img 							#creates img folder
+cd img 								#goes into img folder
+cd ../								#goes back a folder
+mkdir js 							#creates js folder
+cd js 								#goes into js folder
+touch pg1.js 						#creates pg1.js file
+touch pg2.js						#creates pg2.js file
+touch pg3.js 						#creates pg3.js file
+cd ../								#goes back a folder
+mkdir extras 						#creates extras folder
+cd extras							#goes into extras folder
+mkdir audio							#creates audio folder
+cd audio							#goes into audio folder
+touch song1.m4a						#creates song1.m4a file
+cd ../								#goes back a folder
+mkdir video							#creates video folder
+cd video 							#goes into video folder
+touch vid1.mp4						#creates vid1.mp4 file
+cd ../../img 						#goes back twice and then go to img folder
+#download an image
+curl -o img1.png "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Son_Goku_YoungAdult.PNG/220px-Son_Goku_YoungAdult.PNG"
+curl -o img2.png "https://t00.deviantart.net/2Gc58trL4uSfUdbZurRkBnk4glE=/fit-in/500x250/filters:fixed_height(100,100):origin()/pre00/ae39/th/pre/i/2018/001/2/5/vegetto_ssj_blue_by_saodvd-dbylft5.png" 
+curl -o img3.png "https://t00.deviantart.net/QNbG4EBcrWFIH9HMXnVyBIBRhYY=/fit-in/500x250/filters:fixed_height(100,100):origin()/pre00/e72b/th/pre/i/2017/080/d/b/goku_ssj2_by_saodvd-db336ev.png"
+
+
